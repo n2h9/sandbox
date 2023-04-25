335085948b28bdfcd3bf1037ceee73c5e0ea55d8
n2 h9 <n2h9z4@gmail.com>
2023-04-25 10:48:18 +0200

    deleted serialisation folder

--- serialisation/errors.go
+++
@@ -1,9 +0,0 @@
-package serialisation
-
-import (
-	"errors"
-)
-
-var ErrInvalidFormat error = errors.New("invalid format")
-
-var ErrConversion error = errors.New("conversion error")
--- serialisation/go.mod
+++
@@ -1,3 +0,0 @@
-module serialisation
-
-go 1.19
