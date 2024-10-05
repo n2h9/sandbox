ed63e1dd057642334416da36e3c1a905530b58f0
n2 h9 <n2h9z4@gmail.com>
2024-10-05 20:07:59 +0400

    cpp: uncomment main

--- cpp_playground_01/executables/hello_world_playground/main.cpp
+++ cpp_playground_01/executables/hello_world_playground/main.cpp
@@ -11,93 +11,92 @@
 #include "push_pop_queue_with_latch.cpp"
 
 int main() {
-  std::cout << "start" << std::endl;
-  // {
-  //   using namespace our_project::tools;
-  //   print_hello();
-  // }
+  {
+    using namespace our_project::tools;
+    print_hello();
+  }
+
+  {
+    using namespace std;
+    cout << "qwerty" << endl;
+  }
+
+  {
+    using namespace std;
+    cout << "jthread " << this_thread::get_id() << endl;
+
+    jthread t1 = jthread{[]() -> void {
+      cout << "inside the thread" << endl;
+      cout << this_thread::get_id() << endl;
+    }};
+
+    // this_thread::sleep_for(chrono::seconds{1});
+    t1.join();
+
+    cout << "end of block" << endl;
+  }
+
+  { push_pop_queue_with_jthread(); }
+
+  {
+    using namespace std;
+    cout << "----- -----" << endl;
+    cout << "start of block" << endl;
+
+    auto divide_lambda = [](int a, int b, promise<int>& result) {
+      if (b == 0) {
+        auto ex = runtime_error("division by zero");
+        result.set_exception(make_exception_ptr(ex));
+        return;
+      }
+
+      result.set_value(a / b);
+    };
+
+    auto p = promise<int>{};
+    auto t1 = thread{
+        divide_lambda,
+        24,
+        0,
+        ref(p),
+    };
+    t1.detach();
+
+    auto f = p.get_future();
+
+    try {
+      auto value = f.get();
+      cout << "value from future = " << value << endl;
+    } catch (exception& ex) {
+      cout << "exception from future = " << ex.what() << endl;
+    }
+
+    cout << "end of block" << endl;
+    cout << "----- -----" << endl;
+  }
 
   // {
   //   using namespace std;
-  //   cout << "qwerty" << endl;
+  //   cout << "increment counter example" << endl;
   // }
 
-  // {
-  //   using namespace std;
-  //   cout << "jthread " << this_thread::get_id() << endl;
+  {
+    using namespace std;
 
-  //   jthread t1 = jthread{[]() -> void {
-  //     cout << "inside the thread" << endl;
-  //     cout << this_thread::get_id() << endl;
-  //   }};
+    cout << "----- -----" << endl;
+    cout << "start of block" << endl;
+    int i{28};
+    decltype(i) j{34};
 
-  //   // this_thread::sleep_for(chrono::seconds{1});
-  //   t1.join();
+    cout << j << endl;
 
-  //   cout << "end of block" << endl;
-  // }
+    push_pop_queue_with_async();
 
-  // { push_pop_queue_with_jthread(); }
+    // packaged_task
 
-  // {
-  //   using namespace std;
-  //   cout << "----- -----" << endl;
-  //   cout << "start of block" << endl;
-
-  //   auto divide_lambda = [](int a, int b, promise<int>& result) {
-  //     if (b == 0) {
-  //       auto ex = runtime_error("division by zero");
-  //       result.set_exception(make_exception_ptr(ex));
-  //       return;
-  //     }
-
-  //     result.set_value(a / b);
-  //   };
-
-  //   auto p = promise<int>{};
-  //   auto t1 = thread{
-  //       divide_lambda,
-  //       24,
-  //       0,
-  //       ref(p),
-  //   };
-  //   t1.detach();
-
-  //   auto f = p.get_future();
-
-  //   try {
-  //     auto value = f.get();
-  //     cout << "value from future = " << value << endl;
-  //   } catch (exception& ex) {
-  //     cout << "exception from future = " << ex.what() << endl;
-  //   }
-
-  //   cout << "end of block" << endl;
-  //   cout << "----- -----" << endl;
-  // }
-
-  // // {
-  // //   using namespace std;
-  // //   cout << "increment counter example" << endl;
-  // // }
-
-  // {
-  //   using namespace std;
-
-  //   cout << "----- -----" << endl;
-  //   cout << "start of block" << endl;
-  //   int i{28};
-  //   decltype(i) j{34};
-
-  //   cout << j << endl;
-
-  //   push_pop_queue_with_async();
-
-  //   // packaged_task
-
-  //   cout << "end of block" << endl;
-  //   cout << "----- -----" << endl;
-  // }
+    cout << "end of block" << endl;
+    cout << "----- -----" << endl;
+  }
 
   { push_pop_queue_with_latch(); }
 
